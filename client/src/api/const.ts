d059a58eda03b25481995c3212bd6e555fafa501
youngcodej22 <youngcodej22@gmail.com>
2023-11-16 15:04:56 +0900

    feat: UPDATE const.ts, login_axios.ts
    테스트를 위한 BASE_URL 변경 및 login_axios 변경

--- client/src/api/const.ts
+++ client/src/api/const.ts
@@ -1,6 +1,9 @@
-// ! 실습서버 login: 
+// ! 실습서버 login:
 // * blue 1234!@#$
 // * red 1234!@#$
 // * white 1234!@#$
 
-export const BASE_URL = 'https://wanted-p2.bluestragglr.com'
+// * Test 실습서버
+// export const BASE_URL = 'https://wanted-p2.bluestragglr.com'
+
+export const BASE_URL = "http://localhost:3500/";
