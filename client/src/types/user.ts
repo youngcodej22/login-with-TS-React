b8b9ec949fc25047e86efe87483ea1b296a606a3
youngcodej22 <youngcodej22@gmail.com>
2023-11-28 14:13:24 +0900

    feat: UPDATE user.ts in type
    타입 추가

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,9 +1,37 @@
-export interface UserInfo {
-  name: string;
+// export interface UserInfo {
+//   name: string;
+// }
+
+type ROLE_USER = "user";
+type ROLE_ADMIN = "admin";
+
+export const UserRole: ROLE_USER = "user";
+export const AdminRole: ROLE_ADMIN = "admin";
+
+export type UserRole = ROLE_USER | ROLE_ADMIN;
+
+export interface UserProfile {
+  userProfile: {
+    username: string;
+    role: string[];
+    // role: UserRole[];
+  };
 }
 
 export interface User {
+  userId: number;
   username: string;
   password: string;
-  userInfo: UserInfo;
+  userProfile: UserProfile;
+}
+
+export interface Item {
+  id: number;
+  owner: {
+    userId: number;
+  };
+  content: {
+    title: string;
+    body: string;
+  };
 }
